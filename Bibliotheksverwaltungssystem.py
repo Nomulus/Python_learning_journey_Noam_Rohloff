57f16d0c8811351a76ab347fa6df16c313873bab
Nomulus <noamrohloff@gmail.com>
2026-01-02 19:37:29 +0100

    Book und Class Library erstellt

--- Bibliotheksverwaltungssystem.py
+++ Bibliotheksverwaltungssystem.py
@@ -1,3 +1,34 @@
+class Book:
+    def __init__(self, title, author, isbn, is_available):
+        self.title = title
+        self.author = author
+        self._isbn = isbn
+        self.is_available = is_available
+
+    @property
+    def isbn(self):
+        return self._isbn
+    
+    @isbn.setter
+    def isbn(self, value):
+        self._isbn = value
+
+class Library:
+    def add_book(self, book):
+        ...
+    
+    def find_book_by_isbn(self, isbn):
+        ...
+
+    def list_available_bookd(self):
+        ...
+    
+    def borrow_book(isbn):
+        ...
+
+    def return_book(isbn):
+        ...
+
 def main():
     ...
 
