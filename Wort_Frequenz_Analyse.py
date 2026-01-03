17e795e2e3652aa1e20d88f1508822b7f180c83e
Nomulus <noamrohloff@gmail.com>
2026-01-03 16:00:43 +0100

    made it output the analized text to print and file

--- Wort_Frequenz_Analyse.py
+++ Wort_Frequenz_Analyse.py
@@ -1,7 +1,10 @@
+from collections import Counter
+
 class TextAnalyzer():
     def __init__(self, text):
         words = self.analyze(text)
-    
+        self.counter = Counter(words)
+
     def analyze(self, text):
         text = text.lower()
         words = text.split(" ")
@@ -9,7 +12,7 @@ class TextAnalyzer():
         for word in words:
             for zeichen in self.sonderzeichen:
                 if zeichen in word:
-                    word = word.replace(zeichen, "")
+                    word = word.strip(zeichen)
             
             found_stop = False
             for stop in self.stopwords:
@@ -18,22 +21,41 @@ class TextAnalyzer():
                     break
             if not found_stop:
                 text_ohne_sonderzeichnen_und_stopwords.append(word)
-        print(text_ohne_sonderzeichnen_und_stopwords)
+        return text_ohne_sonderzeichnen_und_stopwords
+    
+    def __str__(self):
+        return (self.concatenate_counter())
+    
+    def concatenate_counter(self):
+        zeilen = []
+        for i , pair in enumerate(self.counter.most_common(10)):
+            if i == 0:
+                zeilen.append(f"\n{pair[0].title()} wurde mit {pair[1]} Wierderholungen am meisten wiederholt!")
+            else:
+                zeilen.append(f"{i+1}: {pair[0]}: {pair[1]} Wiederholungen")
+        return "\n".join(zeilen)
 
     @property
     def sonderzeichen(self):
-        return [".", ",", "!", "?", ";", ":"]
+        return {".", ",", "!", "?", ";", ":"}
     
     @property
     def stopwords(self):
-        return ['der', 'die', 'das', 'dem', 'den', 'des', 'ein', 'eine', 'einer', 'einem', 'einen', 'eines',
+        return {'der', 'die', 'das', 'dem', 'den', 'des', 'ein', 'eine', 'einer', 'einem', 'einen', 'eines',
         'und', 'in', 'zu', 'von', 'mit', 'sich', 'auf', 'ist', 'es', 'nicht', 'an', 'für', 'am', 'im',
-        'doch', 'jeder', 'jedes', 'einmal', 'erst', 'als']
+        'doch', 'jeder', 'jedes', 'einmal', 'erst', 'als'}
         
 
 
 def main():
     text_analyzer = TextAnalyzer("Das Haus ist groß, und in dem Haus ist es schön! Doch nicht jeder in dem Haus ist glücklich. Das Glück ist ein flüchtiges Gut, besonders in einem Haus, das mit Sorgen gefüllt ist. Ist das Haus erst einmal leer, ist es still.")
+    print(text_analyzer)
+    write_to_file(text_analyzer, "top_words.txt")
+
+def write_to_file(to_write, filename):
+    with open(filename, "w") as file:
+        file.write(to_write)
+
 
 if __name__ == "__main__":
     main()
