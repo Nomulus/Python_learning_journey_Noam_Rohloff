a77c840d409019d0983faf4e10ff88ab27391647
Nomulus <noamrohloff@gmail.com>
2026-01-03 17:37:43 +0100

    alle Tests geschrieben

--- test_Wort_Frequenz_Analyse.py
+++ test_Wort_Frequenz_Analyse.py
@@ -6,3 +6,20 @@ def standard_text_analyzer():
     text_analyzer = TextAnalyzer("Das Haus ist groß, und in dem Haus ist es schön! Doch nicht jeder in dem Haus ist glücklich. Das Glück ist ein flüchtiges Gut, besonders in einem Haus, das mit Sorgen gefüllt ist. Ist das Haus erst einmal leer, ist es still.")
     return text_analyzer
 
+def test_str(standard_text_analyzer):
+    assert standard_text_analyzer.__str__() == standard_text_analyzer.concatenate_counter()
+    
+def test_analyze_stopword_removal(standard_text_analyzer):
+    assert standard_text_analyzer.analyze("Ich will den haben") == ["ich", "will", "haben"]
+    assert standard_text_analyzer.analyze("Ich der den dann den haben") == ["ich", "dann", "haben"]
+
+def test_analyze_sonderzeichen_removal(standard_text_analyzer):
+    assert standard_text_analyzer.analyze("Ich!!!! will !!jetzt!!!!!") == ["ich", "will", "jetzt"]
+    assert standard_text_analyzer.analyze("Ich.,!! will !!jetzt!!!!.") == ["ich", "will", "jetzt"]
+
+def test_analyze_if_no_text_given(standard_text_analyzer):
+    assert standard_text_analyzer.analyze("") == []
+
+def test_analyze_raises_error(standard_text_analyzer):
+    with pytest.raises(TypeError):
+        standard_text_analyzer.analyze()
