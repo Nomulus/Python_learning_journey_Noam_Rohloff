1af6ceb6e50cbd9f3bf974b030c115e70cf07274
Nomulus <noamrohloff@gmail.com>
2026-01-04 21:29:59 +0100

    modlarer umgeschrieben

--- Krypto_Tracker.py
+++ Krypto_Tracker.py
@@ -4,32 +4,41 @@ import json
 import time
 
 def main():
-    no_prior_data = False
-    try:
-        loaded_price, loaded_date = load_price_and_date()
-    except FileNotFoundError:
-        no_prior_data = True
-        print("No prior data to compare to, will just output current data.")
-    price_now, time_now = write_current_price_and_date()
+    loaded_price, loaded_date = try_load_data("price_over_time.json")
+    if not loaded_price and not loaded_date:
+        loaded_price, loaded_date = 72262, datetime.datetime.fromisoformat("2026-01-04 21:10:06.271862")
 
-    if no_prior_data:
-        print(f"Current price of Bitcoin: {price_now}CHF\nTime: {time_now}")
-    else:
-        compare_data(loaded_price, loaded_date, price_now, time_now)
+    price_now, time_now = get_current_price_and_date()
 
-def write_current_price_and_date():
+    write_current_price_and_date(price_now, time_now)
+
+    compare_data(loaded_price, loaded_date, price_now, time_now)
+
+def get_current_price_and_date():
     request = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=chf").json()
     try:
         price_now = (request["bitcoin"]["chf"],)[0]
     except KeyError:
         raise SystemExit("rate Limit has been exceeded.")
-    time_now = datetime.datetime.now().isoformat()
-    with open("price_over_time.json", "w") as file:
-        json.dump([price_now, time_now], file)
-    return price_now, datetime.datetime.now()
+    time_now = datetime.datetime.now()
+    return price_now, time_now
 
-def load_price_and_date():
-    with open("price_over_time.json", "r") as file:
+def write_current_price_and_date(price_now, time_now):
+    with open("price_over_time.json", "w") as file:
+        json.dump([price_now, time_now.isoformat()], file)
+
+def try_load_data(file_name):
+    try:
+        loaded_price, loaded_date = load_price_and_date(file_name)
+    except FileNotFoundError:
+        print("No prior data to compare to, will output with old data")
+        loaded_price, loaded_date = None, None
+    return loaded_price, loaded_date
+
+
+
+def load_price_and_date(file_name):
+    with open(file_name, "r") as file:
         data = json.load(file)
         return data[0], datetime.datetime.fromisoformat(data[1])
 
