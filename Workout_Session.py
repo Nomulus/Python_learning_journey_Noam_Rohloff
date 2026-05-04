933adc7b6f9243f6015828b880dbd0dccd24693c
Noam <noamrohloff@gmail.com>
2026-05-04 21:31:18 +0200

    started building the base logic for inputting your training as you go

--- Workout_Session.py
+++ Workout_Session.py
@@ -1,7 +1,30 @@
 import datetime
+import sys
 
 class Workout_Session:
     def __init__(self, muscle_group, date = datetime.datetime.now()) -> None:
         self._muscle_group = muscle_group
         self.date = date
-        
+        self.train()
+    
+    def __str__(self):
+        return str(self.date)
+    
+    def train(self):
+        while True:
+            try:
+                self.excercise()
+            except SystemExit:
+                sys.exit #add by by
+    
+    def excercise(self):
+        print("excercise, weight, reps")
+        usr_input = input().split(",")
+        print(usr_input)
+
+def main():
+    Workout = Workout_Session
+    print(Workout)
+
+if __name__ == "__main__":
+    main()
