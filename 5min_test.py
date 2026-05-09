df35535ddbe3bb6e13c59249ef25c4b2df1f8291
Nomulus <noamrohloff@gmail.com>
2026-05-09 12:08:01 +0200

    small test for me

---
+++ 5min_test.py
@@ -0,0 +1,8 @@
+raw_users = ["  Alice", "bob", "ALICE", "charles", "  bob  ", "Alice", "David"]
+
+# Aufgabe: 
+# Erstelle eine neue Liste, in der jeder Name:
+# 1. Keine Leerzeichen am Anfang oder Ende hat.
+# 2. Nur der erste Buchstabe groß ist (Proper Case).
+# 3. Jeder Name nur genau EINMAL vorkommt.
+# 4. Die Liste am Ende alphabetisch sortiert ist.
