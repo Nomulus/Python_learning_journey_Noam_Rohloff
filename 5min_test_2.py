0adebf13e9eafda1a321b7f936fc5af5eb8e537d
Nomulus <noamrohloff@gmail.com>
2026-05-10 16:57:04 +0200

    nice job, funky if statements, rest looks good

--- 5min_test_2.py
+++ 5min_test_2.py
@@ -4,5 +4,36 @@ Alle Werte unter 0 sollen ignoriert werden (Messfehler).
 
 Falls ein Wert 100 kV überschreitet, soll eine Warnung ausgegeben werden.
 
-Berechne den Durchschnitt der gültigen Werte."""
+Berechne den Durchschnitt der gültigen Werte. 
 
+messwerte = [12.5, -1.0, 95.0, 110.2, 88.7]"""
+
+def main():
+    messwerte = [12.5, -1.0, 95.0, 110.2, 88.7]
+
+    print(validate_measurements(messwerte))
+    
+
+def validate_measurements(messwerte):
+    """Function to validate measurements and warn of high voltages
+    
+    param messwerte: list = List of measurements to be evaluated
+    return clean: list = list of all the clean measurements
+    """
+
+    clean = []
+    for measurement in messwerte:
+        if measurement <0:
+            pass
+        else:
+            clean.append(measurement)
+        
+        if measurement > 100:
+            print(f"warning, {measurement} kV.")
+    
+    return clean
+
+
+
+if __name__ == "__main__":
+    main()
