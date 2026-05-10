296d9323c4bcc797894fb40e4aad5582ce668bef
Nomulus <noamrohloff@gmail.com>
2026-05-10 16:48:47 +0200

    new little test

---
+++ 5min_test_2.py
@@ -0,0 +1,8 @@
+"""Wir haben eine Liste mit Spannungsmesswerten in Kilovolt (kV) erhalten. Schreibe eine Funktion, die diese Liste bereinigt:
+
+Alle Werte unter 0 sollen ignoriert werden (Messfehler).
+
+Falls ein Wert 100 kV überschreitet, soll eine Warnung ausgegeben werden.
+
+Berechne den Durchschnitt der gültigen Werte."""
+
